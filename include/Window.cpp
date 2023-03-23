09f11a3ef91852051e84878575e2fcfa7797b14a
dirii <minhquang116@gmail.com>
2023-03-23 10:36:02 +0700

    create window header file

---
+++ include/Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dirii on 23/03/2023.
+//
+
+#include "Window.h"
---
+++ include/Window.h
@@ -0,0 +1,8 @@
+//
+// Created by dirii on 23/03/2023.
+//
+
+#ifndef VISUALGO_CS162_WINDOW_H
+#define VISUALGO_CS162_WINDOW_H
+
+#endif //VISUALGO_CS162_WINDOW_H
